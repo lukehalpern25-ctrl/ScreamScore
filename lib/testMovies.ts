fed8652d963ffc730cf1bb512a317359906e44fe
soso <sophiatardieux@sosos-MacBook-Air.local>
2026-01-22 10:19:37 -0800

    Add testMovies.ts with sample movies

--- lib/testMovies.ts
+++ lib/testMovies.ts
@@ -1,5 +1,6 @@
 // lib/testMovies.ts
 
+<<<<<<< HEAD
 export const testMovies = [
   {
     id: 2001,
@@ -55,4 +56,14 @@ export const testMovies = [
     poster: "/nightmare-on-elm-street.jpg",
     rating: 9,
   },
+=======
+  // Slashers & Thrillers
+  { id: 3001, title: "The Texas Chainsaw Massacre", category: "Slashers & Thrillers", genres: "Horror, Slasher, Thriller", releaseDate: "1974", poster: "/texas-chainsaw-massacre.jpg", rating: 9 },
+  { id: 3002, title: "Halloween", category: "Slashers & Thrillers", genres: "Horror, Slasher, Thriller", releaseDate: "1978", poster: "/halloween.jpg", rating: 9 },
+  { id: 3003, title: "Final Destination", category: "Slashers & Thrillers", genres: "Horror, Thriller, Slasher", releaseDate: "2000", poster: "/final-destination.jpg", rating: 8 },
+  { id: 3004, title: "Saw", category: "Slashers & Thrillers", genres: "Horror, Thriller, Slasher", releaseDate: "2004", poster: "/saw.jpg", rating: 8 },
+  { id: 3005, title: "Friday the 13th", category: "Slashers & Thrillers", genres: "Horror, Slasher, Thriller", releaseDate: "1980", poster: "/friday-13th.jpg", rating: 8 },
+  { id: 3006, title: "A Nightmare on Elm Street", category: "Slashers & Thrillers", genres: "Horror, Slasher, Thriller", releaseDate: "1984", poster: "/nightmare-on-elm-street.jpg", rating: 9 },
+  { id: 3007, title: "Childs Play", category: "Slashers & Thrillers", genres: "Horror, Slasher, Thriller", releaseDate: "1988", poster: "/childs-play.jpg", rating: 8 },
+>>>>>>> 546d0dd (Add testMovies.ts with sample movie data)
 ];
